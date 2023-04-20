eea589390a87be875bfc4ce0ea3bbca36ce34184
Abhay <abhaykhemnar07@gmail.com>
2023-04-20 08:25:41 +0530

    all in app,js file no component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Benifits of using react...</h1>
+      <ol>
+        <li> 1.Component-based Architecture </li>
+        <li> 2.Virtual DOM for efficient updates</li>
+        <li> 3.Rich ecosystem and community </li>
+        <li> 4.cross platform development </li>
+        <li> 5.Strong community support </li>
+      </ol>
+      <a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+  >
+  <button>
+     GEt START...
+    </button>
+</a>
+    
     </div>
   );
 }
 
 export default App;
+//<header className="App-header">
+/* <img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */
