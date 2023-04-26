034d789d11fb404399e5993e223d50ce7110ccb3
Abhay <abhaykhemnar07@gmail.com>
2023-04-26 23:27:22 +0530

    use useState Function In OnClick.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import Card from './Component/Card/card';
+import OnClick from './OnClick/Onclick';
 
 function App() {
- 
-//  const CardOne ={
-//      Name:"afhfjjh",
-//      img:"https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-//      Describtion:"sport shoes",
-//      price:60,
-//  }
-
-
   return (
     <div className="App">
-      <div class="First_row">
-       <Card img="https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        Name="shoe"
-        Describtion="Sport shoe" 
-        price ="10"
-        rating="5" />
-
-          <Card img="https://images.pexels.com/photos/277319/pexels-photo-277319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        Name="watch"
-        Describtion="Classic watch" 
-        price ="50"
-        rating="5" />
-
-          <Card img="https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        Name="T-Shirt"
-        Describtion="casual Shirt" 
-        price ="13"
-        rating="4" />
-    
+   
+   <OnClick/>
     </div>
-    <div class="Second_row">
-    <Card img="https://images.pexels.com/photos/2313192/pexels-photo-2313192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        Name="shirt"
-        Describtion="casusal shirt" 
-        price ="10"
-        rating="5" />
-        
-          <Card img="https://images.pexels.com/photos/701877/pexels-photo-701877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        Name="sun glass"
-        Describtion="casual Sun glass" 
-        price ="15"
-        rating="5" />
-    </div>
-  </div>
   );
 }
 
---
+++ src/OnClick/Onclick.js
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import './Onclick.css'
+
+export default function OnClick(){
+
+    let [count , setCount]= useState(1)
+    function Incress(){
+       let newcount=count+1;
+       setCount(newcount)
+    }
+
+    function Decress(){
+        if(count>0){
+        let newcount=count-1;
+        setCount(newcount)
+     }if(count===0){
+        alert("Can not go below 0")
+     }
+    }
+
+
+    return(
+
+        <div class ="contain">
+            <p class="Count">{count}</p>
+            <button onClick={Incress}>Click me to Incress</button>
+            <br/>
+            <button onClick={Decress}>Click me to decress</button>
+        </div>
+        
+        
+        
+    )
+}
