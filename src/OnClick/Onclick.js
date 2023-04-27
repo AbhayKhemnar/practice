6551063919ba5799ce09905b4418bcce6ad4a8e1
Abhay <abhaykhemnar07@gmail.com>
2023-04-27 17:43:07 +0530

    Created Game In OnClick Component in Onclick.js file

--- src/OnClick/Onclick.js
+++ src/OnClick/Onclick.js
@@ -3,26 +3,39 @@ import './Onclick.css'
 
 export default function OnClick(){
 
-   let [Name , setName]= useState()
-   let [UpdatedName , setUpdatedName]=useState("________")
+   let [number , setNumber]=useState()
+   let [Attempt , setAttempt] = useState(1)
+   let [LuckyNumber,setLuckyNumber]=useState(Math.round((Math.random()*10)))
+
+  console.log(LuckyNumber)
  
+  function GetInput(e){
+        setNumber(e.target.value)
+  }
+    
+  function HandleInput(){
+    if(number<0 || number>10){
+        alert("WARNING: Enter number between 0 and 10")
+        return
+    }
+   setAttempt(Attempt+1)
+      if(number==LuckyNumber){
+        alert(' CONGRATS YOU Won IN  "'+ Attempt + '"  ATTEMPT '  )
+      }else if(number<LuckyNumber){
+        alert("you Number is less than Lucky Number")
+      }else{
+        alert("You number is greater than Lucky Number")
+      }
+  
+  } 
 
-   function DisplayName(){
-        setUpdatedName(Name)
-         
-   }
-
-   function handleName(event){
-    setName(event.target.value)
-      
-   }
 
     return(
 
         <div class ="contain">
-            <input onChange={handleName} placeholder="Enter Your Name" ></input>
-            <button onClick={DisplayName} >Summit</button>
-            <h1>Your Name is {UpdatedName}</h1>
+            <h3>Number is between  0 to 10</h3>
+            <input onChange={GetInput} placeholder="Guess Lucky Number" type="number" ></input>
+            <button onClick={HandleInput} value ="10"> Match Number </button>
         </div>
         
         
