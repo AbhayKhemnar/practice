ca9a1ceecf5c2135433f2c2813fe9879c8bf65f4
Abhay <abhaykhemnar07@gmail.com>
2023-05-03 10:54:25 +0530

    card is created in onclick.js and api is also fetch

--- src/OnClick/Onclick.js
+++ src/OnClick/Onclick.js
@@ -1,31 +1,56 @@
-import { useState,useEffect} from "react";
+import { useState} from "react";
 import './Onclick.css'
 
 
 
 export default function OnClick(){
 
+  let [Data,setData]=useState([])
 
-  const [imageUrl, setImageUrl] = useState("");
 
-  useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((data) => {
-        setImageUrl(data.message);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [imageUrl]);
+  fetch("https://reqres.in/api/users/")
+  .then((responce)=>responce.json())
+  .then((data)=>setData(data.data))
+  .catch((error)=>console.log(error))
+   console.log(Data)
+     
 
-  return (
-    <div>
-      <img 
-      height={200}
-      src={imageUrl} alt="dog" />
-    </div>
-  );
+
+return(
+  <div>
+<div class="container">
+  {
+    Data.map((x)=>((
+       <div class="card">
+          <div class="Upper_section">
+              <img
+              class="img"
+              height={100}
+              width={100}
+              src={x.avatar} 
+              alt={x.first_name}
+              />
+          </div>
+          <div class="Lower_section">
+              <h3>id :{x.id}</h3>
+              <h1>{x.first_name }   {x.last_name}</h1>
+              <h3>Email:{x.email}</h3>
+           </div>
+
+        </div>
+  ))
+    )
+
+
+
 }
-             
-    
+
+</div>
+<div>
+</div>
+</div>
+)
+
+
+
+}
